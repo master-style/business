451f76f5a6539161cea24f5c014251367cc5541c
BenSeage <bg012300@gmail.com>
2021-05-06 09:31:47 +0800

    fix: is-in / is-enum

--- src/utils/is-enum.ts
+++ src/utils/is-enum.ts
@@ -1,14 +1,16 @@
 import { InputCustomValidate } from '../interfaces/input-custom-validate';
 
-export function IsEnum(enumObj): InputCustomValidate {
+export function IsEnum(enumObj, each?: boolean): InputCustomValidate {
     const values = Object
         .keys(enumObj)
         .map(key => enumObj[key]);
 
     return {
-        message: `$field must be in [` + values.join(', ') + `]`,
+        message: `$field must be in [${values.join(', ')}]` + (each ? ' array' : ''),
         validate: (value) => {
-            return typeof value === 'boolean';
+            return each
+                ? value.every(eachValue => values.includes(eachValue))
+                : values.includes(value);
         }
     };
 }
